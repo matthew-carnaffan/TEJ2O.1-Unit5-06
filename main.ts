af661c5759f1f27cc15c0d2c7405d93bb740d1fa
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-03-25 12:57:35 -0400

    I wrote code for to work

--- main.ts
+++ main.ts
@@ -5,3 +5,26 @@
  * This program controls a servo
 */
 
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+  robotbit.Servo(servoNumber1, 0)
+  basic.clearScreen()
+  basic.showNumber(0)
+  basic.showIcon(IconNames.SmallSquare)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+  robotbit.Servo(servoNumber1, 180)
+  basic.clearScreen()
+  basic.showNumber(180)
+  basic.showIcon(IconNames.SmallSquare)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
